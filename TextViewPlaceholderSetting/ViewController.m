f37c0f1573362a890d67f1773cdaf3c257b43369
MrBraveChenBinBin <chenbinbin@chenbinemacbook.lan>
2016-09-21 15:34:26 +0800

    2016年09月21日15:34:24

--- TextViewPlaceholderSetting/ViewController.m
+++ TextViewPlaceholderSetting/ViewController.m
@@ -7,8 +7,8 @@
 //
 
 #import "ViewController.h"
-
-@interface ViewController ()
+#import "CBBTextView.h"
+@interface ViewController ()<UITextViewDelegate>
 
 @end
 
@@ -16,14 +16,39 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    CBBTextView * textView =[[CBBTextView alloc] initWithFrame:CGRectMake(10, 100, [UIScreen mainScreen].bounds.size.width - 20, 180)];
+    textView.backgroundColor = [UIColor whiteColor];
+    textView.placeholderColor = [UIColor lightGrayColor];
+    textView.placeholder = @"您的意见是我们前进的最大动力，谢谢！";
+    textView.font = [UIFont systemFontOfSize:17];
+    textView.delegate = self;
+    textView.layer.borderWidth =1;
+    textView.layer.borderColor =[UIColor redColor].CGColor;
+    [textView.layer setCornerRadius:10.0f];
+    [self.view addSubview:textView];
+    
 }
 
+- (void)textViewDidChange:(CBBTextView *)textView
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+{
+    
+    if([textView.placeholder length] == 0)
+    {
+        return;
+    }
+    if([textView.placeholder length]  == 0)
+    {
+        [textView.placeHolderLabel setAlpha:1];
+    }
+    else
+    {
+        [textView.placeHolderLabel  setAlpha:0];
+    }
+    
+    if ([textView.text isEqualToString:@""]) {
+        [textView.placeHolderLabel setAlpha:1];
+    }
 }
-
 
 @end
